fd7f54eb965a3a05116e60386eee6696e21cbc63
unknown <framiomedia@gmail.com>
2026-04-23 19:16:32 +0300

    Attempt to fix admin JS module resolution and bump to 1.0.6

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -3,12 +3,8 @@ const path = require('path');
 
 const flarumConfig = config();
 
-// Giriş noktaları (Entry Points)
 flarumConfig.entry = {
-    // Admin Paneli
     admin: path.resolve(__dirname, './src/admin/index.js'),
-    
-    // Forum (Site Arayüzü) - BURAYI EKLEDİK
     forum: path.resolve(__dirname, './src/forum/index.js')
 };
 
